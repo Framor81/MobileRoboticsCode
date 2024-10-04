b3064b80c84df21636e89bd6626abc5db1a91a67
Framor81 <francisco.moralespuente2022@gmail.com>
2024-10-03 23:02:10 -0700

    AWD

--- Exercises/display/display.h
+++ Exercises/display/display.h
@@ -6,22 +6,23 @@
 U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
 
 class Display{
-    private:
+    // private:
         // const int SDA_PIN; 
         // const int SCL_PIN;
         // bool inverted;
 
-    Display(int sda_pin, int scl_pin, bool inverted) : SDA_PIN(sda_pin), SCL_PIN(scl_pin), inverted(inverted) {};
-    
-    void setup() {
-        u8x8.begin();
-    }
+    // Display(int sda_pin, int scl_pin, bool inverted) : SDA_PIN(sda_pin), SCL_PIN(scl_pin), inverted(inverted) {};
+    public:
+        void setup() {
+            u8x8.begin();
+            u8x8.setFlipMode(1);
+        }
 
-    void loopStep(bool isEnabled, String message) {
-        u8x8.setFont(u8x8_font_chroma48medium8_r);
-        u8x8.setCursor(8, 16);
-        u8x8.print(message);
-    }
+        void loopStep(bool isEnabled, String message) {
+            u8x8.setFont(u8x8_font_chroma48medium8_r);
+            u8x8.setCursor(8, 16);
+            u8x8.print(message);
+        }
 }
 
 
--- Exercises/display/display.ino
+++ Exercises/display/display.ino
@@ -7,3 +7,31 @@ const char* SSID = "Pomona";
 const uint16_t PORT = 8181;
 const unsigned long HEARTBEAT_INTERVAL = 1000;
 WSCommunicator wsCommunicator(SSID, PORT, HEARTBEAT_INTERVAL);
+
+// Initialize display
+Display display;
+
+//
+// Setup
+//
+
+void setup() {
+    Serial.begin(115200);
+    wsCommunicator.setup();
+    display.setup();
+}
+
+//
+// Loop
+//
+
+void loop() {
+    auto wrappedCB = [this](uint8_t num, WStype_t type, uint8_t* payload, size_t length) {
+      wsEventCB(*this, num, type, payload, length);
+    };
+    webSocket.begin();
+    webSocket.onEvent(wrappedCB);
+    // Serial.printf("[COMMUNICATOR::SETUP] WebSocket server at ws://%d.%d.%d.%d:%d\n", ip[0], ip[1], ip[2], ip[3], port);
+
+    display.loopStep("%d.%d.%d.%d:%d\n", ip[0], ip[1], ip[2], ip[3], PORT);
+}
