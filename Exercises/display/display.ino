4484a34f15de34481feaec9b9f5d613ff60e41fa
ksantoso2 <130810746+ksantoso2@users.noreply.github.com>
2024-10-10 10:36:34 -0700

    cleanup

--- Exercises/display/display.ino
+++ Exercises/display/display.ino
@@ -28,13 +28,8 @@ void setup() {
 //
 
 void loop() {
-
-    // u8x8.setFont(u8x8_font_chroma48medium8_r);
-    // u8x8.drawString(0, 0, wsCommunicator.getIpAddress().c_str());
-
     char port[7];
     snprintf(port, 6, ":%d", wsCommunicator.getPort());
-    // u8x8.drawString(0, 1, port);
     
     display.loopstep(0, 0, wsCommunicator.getIpAddress().c_str());
     display.loopstep(0, 1, port);
