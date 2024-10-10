d6eb243abfa9d63fc6980bf34c033b81fb1fd9c4
ksantoso2 <130810746+ksantoso2@users.noreply.github.com>
2024-10-10 09:47:59 -0700

    fix exercise 6

--- Exercises/display/display.h
+++ Exercises/display/display.h
@@ -3,10 +3,11 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
 
 class Display{
     public:
+        U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // OLEDs without Reset of the Display
+
         void setup() {
             u8x8.begin();
             u8x8.setFlipMode(1);
